0d6602cece554635aae0fb6aaeb235a68b7e92b3
Ryan Lynn Rosas <rltrosas88@gmail.com>
2021-07-09 03:02:44 -0500

    connect to the mysql database

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-
+const mysql = require('mysql2');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -7,6 +7,19 @@ const app = express();
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// Connect to database
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // Your MySQL username,
+      user: 'root',
+      // Your MySQL password
+      password: 'Blue1234',
+      database: 'emtracker'
+    },
+    console.log('Connected to the employee tracker database.')
+  );
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
