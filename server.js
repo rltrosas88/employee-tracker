8810397bb8671932d1b1f597613ddc4dbd6d41f6
Ryan Lynn Rosas <rltrosas88@gmail.com>
2021-07-09 12:29:48 -0500

    node_modules

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-
+const db = require('./db/connection');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -14,7 +14,12 @@ app.use(express.json());
 app.use((req, res) => {
     res.status(404).end();
 });
-  
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+
+//Start server after DB connection
+db.connect(err => {
+    if (err) throw err;
+    console.log('Database connected.');
+    app.listen(PORT, () => {
+        console.log('Server running on port ${PORT}');
+    });
 });
